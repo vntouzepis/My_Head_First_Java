8dea5ff85ec154e86dd269e45e14abcec8c73f87
test vasilis <vntouzepis1981@gmail.com>
2020-06-09 23:39:00 +0300

    2nd commit

--- Section_1/MyFirstApp.java
+++ Section_1/MyFirstApp.java
@@ -1,7 +1,7 @@
 public class MyFirstApp{
 	public static void main (String[] args){
 		System.out.println("I Rule!");
-				System.out.print("The World");
+				System.out.print("The World!!");
 				
 				int x=4; //assign 4 to x
                 while (x>3){
